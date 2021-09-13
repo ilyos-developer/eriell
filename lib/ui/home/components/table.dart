90e64c3c6a46dce1c80d5138deac86e30b60cdbd
ilyos <matnazarovilyos@gmail.com>
2021-09-13 11:28:23 +0500

    date table

--- lib/ui/home/components/table.dart
+++ lib/ui/home/components/table.dart
@@ -1,3 +1,4 @@
+import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,82 +8,53 @@ class MyDataTable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scrollbar(
-      child: SingleChildScrollView(
-        child: Scrollbar(
-          isAlwaysShown: true,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: DataTable(
-              sortColumnIndex: 0,
-              headingRowHeight: 50,
-              dataRowColor: MaterialStateProperty.all(Colors.grey[300]),
-              headingRowColor: MaterialStateProperty.all(Colors.grey),
-              columns: [
-                DataColumn(
-                  label: Text("Названия"),
-                ),
-                DataColumn(
-                  label: Text("2010"),
-                ),
-                DataColumn(
-                  label: Text("2011"),
-                ),
-                DataColumn(
-                  label: Text("2012"),
-                ),
-                DataColumn(
-                  label: Text("2013"),
-                ),
-                DataColumn(
-                  label: Text("2014"),
-                ),
-                DataColumn(
-                  label: Text("2015"),
-                ),
-                DataColumn(
-                  label: Text("2016"),
-                ),
-              ],
-              rows: [
-                DataRow(
-                  selected: true,
-                  cells: [
-                    DataCell(Text("Наша проходка")),
-                    DataCell(Text("199838")),
-                    DataCell(Text("434826")),
-                    DataCell(Text("543461")),
-                    DataCell(Text("750539")),
-                    DataCell(Text("825995")),
-                    DataCell(Text("871525")),
-                    DataCell(Text("941718")),
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(Text("Количество пробуренных скважин")),
-                    DataCell(Text("86")),
-                    DataCell(Text("156")),
-                    DataCell(Text("169")),
-                    DataCell(Text("217")),
-                    DataCell(Text("254")),
-                    DataCell(Text("274")),
-                    DataCell(Text("285")),
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(Text("Наши бурловые установки")),
-                    DataCell(Text("29")),
-                    DataCell(Text("39")),
-                    DataCell(Text("44")),
-                    DataCell(Text("52")),
-                    DataCell(Text("54")),
-                    DataCell(Text("56")),
-                    DataCell(Text("58")),
-                  ],
-                ),
-              ],
-            ),
+      child: DataTable2(
+        columnSpacing: 12,
+        horizontalMargin: 12,
+        minWidth: 600,
+        sortColumnIndex: 0,
+        headingRowHeight: 70,
+        dataRowColor: MaterialStateProperty.all(Colors.grey[300]),
+        headingRowColor: MaterialStateProperty.all(Colors.grey),
+        columns: [
+          DataColumn2(
+            label: Text('Названия'),
+            size: ColumnSize.L,
+          ),
+          DataColumn2(
+            label: Text('2010'),
+            size: ColumnSize.L,
+          ),
+          DataColumn2(
+            label: Text('2011'),
+            size: ColumnSize.L,
+          ),
+          DataColumn2(
+            label: Text('2012'),
+            size: ColumnSize.L,
+          ),
+          DataColumn2(
+            label: Text('2013'),
+            size: ColumnSize.L,
+            numeric: true,
+          ),
+          DataColumn2(
+            label: Text('2014'),
+            size: ColumnSize.L,
+            numeric: true,
+          ),
+        ],
+        rows: List<DataRow>.generate(
+          100,
+          (index) => DataRow(
+            cells: [
+              DataCell(Text('A' * (10 - index % 10))),
+              DataCell(Text('B' * (10 - (index + 5) % 10))),
+              DataCell(Text('C' * (15 - (index + 5) % 10))),
+              DataCell(Text('D' * (15 - (index + 10) % 10))),
+              DataCell(Text('F' * (15 - (index + 10) % 10))),
+              DataCell(Text(((index + 0.1) * 25.4).toString()))
+            ],
           ),
         ),
       ),
